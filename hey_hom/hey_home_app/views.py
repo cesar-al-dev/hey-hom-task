ed6807a4d0e9b4cf9768d28286cb9a8042a5b40b
cesar-al-dev <cesarsanchezal.dev@gmail.com>
2023-12-15 19:28:13 -0600

    Update views.py

--- hey_hom/hey_home_app/views.py
+++ hey_hom/hey_home_app/views.py
@@ -11,22 +11,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class PropertyList(APIView):
-    # authentication_classes = [SessionAuthentication, JWTAuthentication]
-    # permission_classes = [IsAuthenticated]
-
     def get(self, request):
         if request.data:
             return Response({'error': 'GET requests should not include a request body.'}, status=status.HTTP_400_BAD_REQUEST)
         libros = Property.objects.all()
         serializer = PropertySerializer(libros, many=True)
         return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = PropertySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
     
 class PropertyInsert(APIView):
     authentication_classes = [SessionAuthentication, JWTAuthentication]
